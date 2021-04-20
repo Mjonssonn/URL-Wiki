a8884ddd2277062396f0abb3adae3ecbca611513
Mjonssonn <mattiasjonsson@live.se>
2021-04-20 20:40:15 +0200

    Test

--- rest-service/src/main/java/com/urlwiki/controller/ArticleController.java
+++ rest-service/src/main/java/com/urlwiki/controller/ArticleController.java
@@ -1,5 +1,55 @@
 package com.urlwiki.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping 
 public class ArticleController {
+	
+	
+	
+	@GetMapping("/find-article")
+	public String findArticle() {
+		return "HTTP article was searched found";
+	}
+	
+	@GetMapping()
+	public String searchArticle() {
+		return "HTTP article was searched for";
+
+	}
+	
+	
+	@GetMapping()
+	public String readArticle() {
+		return "HTTP article was read";
+
+	}
+	
+	
+	@PostMapping()
+	public String addArticle() {
+		return "HTTP article was added";
+	}
+	
+	
+	@PutMapping()
+	public String updateArticle() {
+		return "HTTP article was updated";
+	}
+	
+	
+	@DeleteMapping()
+	public String deleteArticle() {
+		return "HTTP article was deleted";
+	}
+	
+	
+	
 
 }
--- rest-service/src/main/java/com/urlwiki/controller/UserController.java
+++ rest-service/src/main/java/com/urlwiki/controller/UserController.java
@@ -1,7 +1,32 @@
 package com.urlwiki.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+	
 	private String firstName;
 	private String lastName;
 
+
+	@PostMapping("/create-user")
+	public String createUser() {
+		return "User was created";
+	}
+	
+	@PostMapping("/comment")
+	public String comment() {
+		return "Comment was placed";
+	}
+	
+	@PostMapping("/like")
+	public String like() {
+		return "Like was placed";
+	}
+	
+	
 }
