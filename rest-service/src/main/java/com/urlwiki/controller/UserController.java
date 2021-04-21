98d04c12f695ec9afb59f06758dc9cb87bf49ba1
alinasvetko <alinasvetko@hotmail.com>
2021-04-21 10:50:08 +0200

    GET och SET

--- rest-service/src/main/java/com/urlwiki/controller/UserController.java
+++ rest-service/src/main/java/com/urlwiki/controller/UserController.java
@@ -1,32 +1,37 @@
 package com.urlwiki.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/users")
 public class UserController {
 	
+	public UserController() {
+		public UserController(int id; String firstName; String lastName) {
+			this.id= id;
+			this.firstName= firstName;
+			this.lastName= lastName;
+		}
+		
+	}
+	
+	private int id;
 	private String firstName;
 	private String lastName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
 
-
-	@PostMapping("/create-user")
-	public String createUser() {
-		return "User was created";
-	}
-	
-	@PostMapping("/comment")
-	public String comment() {
-		return "Comment was placed";
-	}
-	
-	@PostMapping("/like")
-	public String like() {
-		return "Like was placed";
-	}
-	
-	
 }
