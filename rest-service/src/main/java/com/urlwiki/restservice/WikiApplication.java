24bb89036b537744821b5ffcd2e67682167dd369
Mjonssonn <77546783+Mjonssonn@users.noreply.github.com>
2021-05-16 21:14:20 +0200

    Revert "Revert "Swagger""
    This reverts commit c26f7ca0944dd28fb06026e8a5066cc422bfbd65.

--- rest-service/src/main/java/com/urlwiki/restservice/WikiApplication.java
+++ rest-service/src/main/java/com/urlwiki/restservice/WikiApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 @ComponentScan({"com.urlwiki.controller", "com.urlwiki.repositories", "com.urlwiki.restservices", "com.urlwiki.services", "com.urlwiki.domain"})
 @EntityScan("com.urlwiki.domain")
 public class WikiApplication {
