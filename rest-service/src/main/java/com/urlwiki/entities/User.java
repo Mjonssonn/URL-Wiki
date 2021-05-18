1e9077921065adbae6c93fade46c2b10b1ec34a0
alinasvetko <alinasvetko@hotmail.com>
2021-05-18 08:39:55 +0200

    Update User.java
    Database info update User.java

--- rest-service/src/main/java/com/urlwiki/entities/User.java
+++ rest-service/src/main/java/com/urlwiki/entities/User.java
@@ -1,8 +1,17 @@
 package com.urlwiki.entities;
 
+import java.sql.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import io.swagger.annotations.ApiModelProperty;
 
 @Entity
+@Table(name="UserID")
 
 
 public class User {
@@ -20,30 +29,59 @@ public class User {
 		this.firstName = firstName;
 		this.lastName = lastName;
 	}
+	
 
-	public int getId() {
-		return id;
-	}  
+	@ApiModelProperty(notes= "The unique identifier of an User")
+	@javax.persistence.Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Column(name= "id")
+	private Long Id;
+	
+	@ApiModelProperty(notes= "The FirstName of the User")
+	@Column(name= "first_name")
+	private String firstname;
+	
+	@ApiModelProperty(notes= "The LastName of the User")
+	@Column(name= "last_name")
+	private String lastname;
+	
+	@ApiModelProperty(notes= "The date the User was created")
+	@Column(name= "created_at")
+	private Date createdAt;
+	
+	@ApiModelProperty(notes= "The date the User was updated")
+	@Column(name="updated_at")
+	private Date updatedAt;
+	
 
-	public void setId(int id) {
-		this.id = id;
+	public Integer getId() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	public String getFirstName() {
-		return firstName;
+	public Object getFirstName() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+	public void setFirstName(Object firstName) {
+		// TODO Auto-generated method stub
+		
 	}
 
-	public String getLastName() {
-		return lastName;
+	public Object getLastName() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setLastName(Object lastName) {
+		// TODO Auto-generated method stub
+		
 	}
+	
+	
+	
+	
 
 	
 }
